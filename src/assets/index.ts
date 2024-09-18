8df58b4dad08dcdb0410f616814723ed690ab4e5
abarisic1509 <abarisic@protonmail.com>
2024-09-18 18:58:45 +0200

    initial setup

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+/* ICONS */
+export { default as BusinessIcon } from "./icons/Business";
+export { default as GamesIcon } from "./icons/Games";
+export { default as HealthIcon } from "./icons/Health";
+export { default as HomeIcon } from "./icons/Home";
+export { default as NetGuideIcon } from "./icons/NetGuide";
+export { default as NewsIcon } from "./icons/News";
+export { default as PropertyTodayIcon } from "./icons/PropertyToday";
+export { default as ScienceIcon } from "./icons/Science";
+export { default as SearchIcon } from "./icons/Search";
+export { default as SportsIcon } from "./icons/Sports";
+export { default as TopListsIcon } from "./icons/TopLists";
+export { default as TvGuideIcon } from "./icons/TvGuide";
+export { default as VaerIcon } from "./icons/Vaer";
