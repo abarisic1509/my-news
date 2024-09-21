18cacf1003802dd9cd47bf371f91779cdd1bde43
abarisic1509 <abarisic@protonmail.com>
2024-09-21 12:36:06 +0200

    pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-	return <>Hello</>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CategoryPage, Homepage } from "./pages";
+
+const App: React.FC = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route element={<Homepage />} path="/" />
+				<Route element={<CategoryPage />} path="/:id" />
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MainWrapper } from "../ui/wrappers";
+
+const Homepage = () => {
+	return <MainWrapper>Homepage</MainWrapper>;
+};
+
+export default Homepage;
